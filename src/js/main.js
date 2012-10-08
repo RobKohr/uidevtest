cc105d1e0b13f7c71f645a7799c791374cf092e6
Rob Kohr <spam@robkohr.com>
2012-10-08 12:04:11 -0700

    original files added to new paths. main.js now loads data file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function(){
+    function processData(data){
+	var objects = data.objects;
+	for(var i = 0; i<objects.length; i++){
+	    var object = objects[i];
+	    console.log(object);
+	}
+    }
+    $.ajax({
+	url: './data/uidevtest-data.js',
+	success: processData,
+	dataType: 'json'
+    });
+});
