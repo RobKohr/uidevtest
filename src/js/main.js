e7eba911a6a608b404ce626a0dc19606191450dd
Rob Kohr <spam@robkohr.com>
2012-10-11 08:07:10 -0700

    finished layout of story

--- src/js/main.js
+++ src/js/main.js
@@ -19,12 +19,75 @@ $(document).ready(function(){
 	}
     }
     
+    function columnSplit(html){
+	first = '';
+	second = '';
+	wc = wordCount(html);
+	half_count = wc/2;
+	paragraphs = html.split('</p>');
+	first = paragraphs[0];
+	for(var i = 0; i<paragraphs.length; i++){
+	    first+='</p>';
+	    if(wordCount(first) > half_count)
+		break;
+	    first += paragraphs[i];
+	}
+	for(var i = i; i<paragraphs.length; i++){
+	    second+=paragraphs[i];
+	}
+	second+='</p>';
+	return [first, second];
+    }
+
+    function wordCount(s){
+	s = s.replace(/(^\s*)|(\s*$)/gi,"");
+	s = s.replace(/[ ]{2,}/gi," ");
+	s = s.replace(/\n /,"\n");
+	return s.split(' ').length;
+    }
+
     function showStory(storyKey){
 	var index = storyKeyToIndex(storyKey);
 	var story = stories[index];
-	var content = templates.story.render({story:story});
+	var content = templates.story.render({story:story, formatDate:formatDate, columnSplit:columnSplit});
 	$('#content').html(content);	
+	hideShowByViewSize();
+	reapplyDropShadows();
+	$('img').load(reapplyDropShadows); 
     }
+
+    function hideShowByViewSize(){
+	$('.dynamic-table').removeClass('.dynamic-table');
+	$('.dynamic-row').removeClass('.dynamic-row');
+	$('.dynamic-cell').removeClass('.dynamic-cell');
+	var width = $(window).width();
+	console.log(width);
+	if(width>480){
+	    $('.wide-view').show();
+	    $('.thin-view').hide();
+	    $('.wide-view-is-table').addClass('dynamic-table');
+	    $('.wide-view-is-row').addClass('dynamic-row');
+	    $('.wide-view-is-cell').addClass('dynamic-cell');
+	}else{
+
+	    $('.wide-view').hide();
+	    $('.thin-view').show();
+	}
+
+	
+    }
+    $(window).resize(hideShowByViewSize);
+
+    function reapplyDropShadows(){
+	$('.drop_shadow_layer').remove();
+	$('.shadow').dropshadow({
+	    shadowColor: '#cccccc',
+	    shadowLayer: -100,
+	    distance:'6px',
+	    blur:'3px'
+	});
+    }
+    $(window).resize(reapplyDropShadows);
 
     function storyKeyToIndex(storyKey){
 	return Number(strReplace('sto', '', storyKey))-1;
@@ -42,7 +105,6 @@ $(document).ready(function(){
     function showList(){
 	var content = templates.list.render({stories:stories, formatDate:formatDate});
 	$('#content').html(content);
-	$('.list .story .thumb').dropShadow();
 	$('.list .story').each(function(){
 	    var s = $(this);
 	    $(this).find('a').each(function(){
@@ -54,7 +116,9 @@ $(document).ready(function(){
 	    $(this).click(function(){
 
 	    });
-	})
+	});
+	reapplyDropShadows();
+	$('img').load(reapplyDropShadows); 
     }
 
     function getStoryKey(){
@@ -66,4 +130,6 @@ $(document).ready(function(){
 	success: processData,
 	dataType: 'json'
     });
+
+
 });
